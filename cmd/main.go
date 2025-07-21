fd675901a0f865fbd6b129d3d99fd70a15bf0cfc
Neil Mahajan <neilsmahajan@gmail.com>
2025-07-20 17:52:31 -0700

    Feat: Implemented looping in input to allow snake to keep moving

--- cmd/main.go
+++ cmd/main.go
@@ -24,22 +24,32 @@ var direction = "still"
 var gamePlaying = true
 
 func main() {
+	fmt.Println("Welcome to the Snake Game!")
+	fmt.Println("Use 'w' or 'k' to move up, 's' or 'j' to move down, 'a' or 'h' to move left, 'd' or 'l' to move right.")
+	fmt.Println("Press 'q' or 'Esc' to quit the game.")
+
+	inputChannel := make(chan input.UserInput)
+	go input.ListenForInput(inputChannel)
+
 	for gamePlaying {
 		board.DrawBoard(width, height, snakePositionX, snakePositionY)
-		inputChannel := make(chan input.UserInput)
-		go input.GetUserInput(direction, inputChannel)
-		userInput := <-inputChannel
-		if userInput.Error != nil {
-			fmt.Printf("Error reading input: %v\n", userInput.Error)
+
+		select {
+		case userInput := <-inputChannel:
+			if userInput.Error != nil {
+				fmt.Printf("Error reading input: %v\n", userInput.Error)
+				gamePlaying = false
+				continue
+			}
+			if !userInput.GamePlaying {
+				gamePlaying = false
+				continue
+			}
+			direction = userInput.Direction
+		default:
+			// Do nothing, keep the snake moving
 		}
-		if !userInput.GamePlaying {
-			gamePlaying = false
-			continue
-		}
-		direction = userInput.Direction
-		//if !gamePlaying {
-		//	break
-		//}
+
 		snakePositionX, snakePositionY, gamePlaying = snake.MoveSnake(snakePositionX, snakePositionY, width, height, direction)
 		time.Sleep(200 * time.Millisecond)
 	}
--- internal/input/input.go
+++ internal/input/input.go
@@ -12,21 +12,34 @@ type UserInput struct {
 	Error       error
 }
 
-func GetUserInput(direction string, inputChannel chan<- UserInput) {
-	char, key, err := keyboard.GetSingleKey()
-	if err != nil {
-		inputChannel <- UserInput{Direction: direction, GamePlaying: false, Error: fmt.Errorf("Error reading input: %v", err)}
-	} else if key == keyboard.KeyEsc || char == 'q' || char == 'Q' {
-		inputChannel <- UserInput{Direction: direction, GamePlaying: false, Error: nil}
-	} else if (char == 'w' || char == 'k') && direction != "down" {
-		inputChannel <- UserInput{Direction: "up", GamePlaying: true, Error: nil}
-	} else if (char == 's' || char == 'j') && direction != "up" {
-		inputChannel <- UserInput{Direction: "down", GamePlaying: true, Error: nil}
-	} else if (char == 'a' || char == 'h') && direction != "right" {
-		inputChannel <- UserInput{Direction: "left", GamePlaying: true, Error: nil}
-	} else if (char == 'd' || char == 'l') && direction != "left" {
-		inputChannel <- UserInput{Direction: "right", GamePlaying: true, Error: nil}
-	} else {
-		inputChannel <- UserInput{Direction: direction, GamePlaying: true, Error: nil}
+func ListenForInput(inputChannel chan<- UserInput) {
+	if err := keyboard.Open(); err != nil {
+		inputChannel <- UserInput{GamePlaying: false, Error: fmt.Errorf("error opening keyboard: %v", err)}
+		return
+	}
+	defer keyboard.Close()
+
+	for {
+		char, key, err := keyboard.GetKey()
+		if err != nil {
+			inputChannel <- UserInput{GamePlaying: false, Error: fmt.Errorf("error reading input: %v", err)}
+			return
+		}
+
+		if key == keyboard.KeyEsc || char == 'q' || char == 'Q' {
+			inputChannel <- UserInput{GamePlaying: false, Error: nil}
+			return
+		}
+
+		switch char {
+		case 'w', 'k':
+			inputChannel <- UserInput{Direction: "up", GamePlaying: true, Error: nil}
+		case 's', 'j':
+			inputChannel <- UserInput{Direction: "down", GamePlaying: true, Error: nil}
+		case 'a', 'h':
+			inputChannel <- UserInput{Direction: "left", GamePlaying: true, Error: nil}
+		case 'd', 'l':
+			inputChannel <- UserInput{Direction: "right", GamePlaying: true, Error: nil}
+		}
 	}
 }
