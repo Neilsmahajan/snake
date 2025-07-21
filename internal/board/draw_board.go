90f888d52a9ad5f42b4c4bb779b4afeebe629981
Neil Mahajan <neilsmahajan@gmail.com>
2025-07-21 10:56:51 -0700

    Feat: Updated name of snakePoints to body

--- internal/board/draw_board.go
+++ internal/board/draw_board.go
@@ -14,13 +14,13 @@ type SnakePoint struct {
 	SnakePositionY int
 }
 
-func DrawBoard(boardDimensions BoardDimensions, snakePoints []SnakePoint) {
+func DrawBoard(boardDimensions BoardDimensions, body []SnakePoint) {
 	fmt.Print("\033[H\033[2J") // Clear the console
 	for y := range boardDimensions.Height {
 		for x := range boardDimensions.Width {
 			if x == 0 || x == boardDimensions.Width-1 || y == 0 || y == boardDimensions.Height-1 {
 				fmt.Print("#")
-			} else if x == snakePoints[0].SnakePositionX && y == snakePoints[0].SnakePositionY {
+			} else if x == body[0].SnakePositionX && y == body[0].SnakePositionY {
 				fmt.Print("0")
 			} else {
 				fmt.Print(" ")
